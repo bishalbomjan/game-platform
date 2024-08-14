8be9c4f233d70dafe3bac8ab3d86445d0d13e783
= <bishalbomjan747@gmail.com>
2024-08-14 10:15:55 +0545

    Showing a spinter while fetching the genres

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import useGenres, { Genre } from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import getCroppedImage from "../services/image-url";
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner />;
+  if (error) return null;
   return (
     <>
       <List>
