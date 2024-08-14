9b3ff31b8c9737a4dcab3ca7ffcd2efd380fdec7
= <bishalbomjan747@gmail.com>
2024-08-14 10:58:55 +0545

    Highlight the selected Genre

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -10,9 +10,10 @@ import {
 import getCroppedImage from "../services/image-url";
 
 interface Props {
-  selectedGenres: (genre: Genre) => void;
+  onSelectedGenres: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ selectedGenres }: Props) => {
+const GenreList = ({ onSelectedGenres, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (isLoading) return <Spinner />;
   if (error) return null;
@@ -27,7 +28,11 @@ const GenreList = ({ selectedGenres }: Props) => {
                 boxSize="32px"
                 borderRadius={8}
               />
-              <Button variant="link" onClick={() => selectedGenres(genre)}>
+              <Button
+                variant="link"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectedGenres(genre)}
+              >
                 {genre.name}
               </Button>
             </HStack>
